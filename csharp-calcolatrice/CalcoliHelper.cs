5557667ccb2bada4c91f7ccb37514a9cf1f6fad9
niccov <niccoventuri27@gmail.com>
2023-09-13 12:56:27 +0200

    Aggiungere i file di progetto.

---
+++ csharp-calcolatrice/CalcoliHelper.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_calcolatrice
+{
+    public static class CalcoliHelper
+    {
+        //metodo per la somma di due int
+        public static int SumOfTwoInt(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        //metodo per la somma di due double
+        public static double SumOfTwoDouble(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        //metodo per la differenza tra due int
+        public static int DifferenceOfTwoInt(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        //metodo per la differenza tra due double
+        public static double DifferenceOfTwoDouble(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        //metodo per la moltiplicazione di due interi
+        public static int MultiplicationOfTwoInt(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        //metodo per la moltiplicazione di due double
+        public static double MultiplicationOfTwoDouble(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        //metodo per calcolare il valore assoluto di un intero
+        public static int AbsoluteValueOfInt(int number)
+        {
+            if(number >= 0)
+            {
+                return number;
+            }
+            else 
+            {
+                return -number;
+            }
+        }
+
+        //metodo per calcolare il valore assoluto di un double 
+        public static double AbsoluteValueOfDouble(double number)
+        {
+            if(number >= 0)
+            {
+                return number;
+            }else
+            {
+                return -number;
+            }
+        }
+
+        //metodo per trovare il minimo tra due interi
+        public static int MinimumValueInt(int number1, int number2)
+        {
+            if(number1 < number2)
+            {
+                return number1;
+            }else
+            {
+                return number2;
+            }   
+        }
+
+        //metodo per trovare il minimo tra due double 
+        public static double MinimumValueDouble(double number1, double number2)
+        {
+            if( number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        //metodo per trovare il massimo tra due interi 
+        public static int MaximumValueInt(int number1, int number2)
+        {
+            if(number1 > number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+
+        //metodo per trovare il massimo tra due double 
+        public static double MaximumValueDouble(double number1, double number2)
+        {
+            if(number1 > number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+        }
+    }
+}
---
+++ csharp-calcolatrice/Program.cs
@@ -0,0 +1,85 @@
+﻿// See https://aka.ms/new-console-template for more information
+using csharp_calcolatrice;
+
+//somma di due interi
+Console.WriteLine("Somma di due interi");
+int sommaInt = CalcoliHelper.SumOfTwoInt(2, 4);
+
+Console.WriteLine(sommaInt);
+Console.WriteLine();
+
+//somma di due double
+Console.WriteLine("Somma di due double");
+double sommaDouble = CalcoliHelper.SumOfTwoDouble(1.2, 0.4);
+
+Console.WriteLine(sommaDouble);
+Console.WriteLine();
+
+//differenza tra due interi
+Console.WriteLine("Differenza tra due interi");
+int differenzaInt = CalcoliHelper.DifferenceOfTwoInt(10, 2);
+
+Console.WriteLine(differenzaInt);
+Console.WriteLine();
+
+//differenza tra due double
+Console.WriteLine("Differenza tra due double");
+double differenzaDouble = CalcoliHelper.DifferenceOfTwoDouble(22.12, 10.1);
+
+Console.WriteLine(differenzaDouble);
+Console.WriteLine();
+
+//moltiplicazione di due interi
+Console.WriteLine("Moltiplicazione di due interi");
+int moltiplicazioneInt = CalcoliHelper.MultiplicationOfTwoInt(12, 3);
+
+Console.WriteLine(moltiplicazioneInt);
+Console.WriteLine();
+
+//moltiplicazione di due double
+Console.WriteLine("Moltiplicazione di due double");
+double moltiplicazioneDouble = CalcoliHelper.MultiplicationOfTwoDouble(9.2, 2.3);
+
+Console.WriteLine(moltiplicazioneDouble);
+Console.WriteLine();
+
+//valore assoluto di un numero intero
+Console.WriteLine("Valore assoluto di un intero");
+int valoreAssolutoInt = CalcoliHelper.AbsoluteValueOfInt(5);
+
+Console.WriteLine(valoreAssolutoInt);
+Console.WriteLine();
+
+//valore assoluto di un double
+Console.WriteLine("Valore assoluto di un double");
+double valoreAssolutoDouble = CalcoliHelper.AbsoluteValueOfDouble(4.2);
+
+Console.WriteLine(valoreAssolutoDouble);
+Console.WriteLine();
+
+//valore minimo tra due interi
+Console.WriteLine("Valore minimo tra due interi");
+int minimoInt = CalcoliHelper.MinimumValueInt(5,8);
+
+Console.WriteLine(minimoInt);
+Console.WriteLine();
+
+//valore minimo tra due double
+Console.WriteLine("Valore minimo tra due double");
+double minimoDouble = CalcoliHelper.MinimumValueDouble(7.2, 10.5);
+
+Console.WriteLine(minimoDouble);
+Console.WriteLine();
+
+//valore massimo tra due interi
+Console.WriteLine("Valore massimo tra due interi");
+int massimoInt = CalcoliHelper.MaximumValueInt(3, 9);
+
+Console.WriteLine(massimoInt);
+Console.WriteLine();
+
+//valore massimo tra due double
+Console.WriteLine("Valore massimo tra due double");
+double massimoDouble = CalcoliHelper.MaximumValueDouble(2.4, 2.7);
+
+Console.WriteLine(massimoDouble);
